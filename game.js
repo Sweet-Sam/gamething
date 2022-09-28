b33381dcc135065ae20261f0199991320ae3e524
Sweet-Sam <101749449+Sweet-Sam@users.noreply.github.com>
2022-09-28 13:27:00 -0300

    endme
    please just do it

---
+++ game.js
@@ -0,0 +1,6 @@
+let background = document.getElementById('background');
+let controlPannel = document.getElementById('controlPannel');
+let upButton =  document.getElementById('upButton');
+let leftButton = document.getElementById('leftButton');
+let rightButton = document.getElementById('rightButton');
+let downButton = document.getElementById('downButton');
